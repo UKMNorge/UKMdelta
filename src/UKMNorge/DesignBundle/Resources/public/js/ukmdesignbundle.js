34b39950e8aa6550032d8ecf3a1286d3a8ef3572
Marius Mandal <mariusmandal@gmail.com>
2015-11-23 16:59:08 +0100

    Restrukturert CSS
    close #151, #104, #85, #84, #33, #17, #16, #12, #98, #74,

--- src/UKMNorge/DesignBundle/Resources/public/js/ukmdesignbundle.js
+++ src/UKMNorge/DesignBundle/Resources/public/js/ukmdesignbundle.js
@@ -1,3 +1,39 @@
+jQuery(document).on('click','#show_kontaktpersoner,#show_main_mobile_menu', function(){
+	pageFocus( jQuery(this) );
+});
+
+function pageFocus(clicked) {
+    if (clicked.attr('data-action') == 'show') {
+        clicked.attr('data-action', 'hide');
+
+		var title = clicked.attr('data-showJumboUKM');
+		if (title == 'true') {
+			jQuery('#ukm_page_jumbo_header').hide();
+			jQuery('#ukm_page_jumbo_header_temp').show();
+		}
+
+        jQuery('#ukm_page_content').hide();
+        jQuery('#ukm_page_pre_content').html( jQuery( clicked.attr('data-toggle') ).html() ).slideDown();
+
+		jQuery('#ukm_page_jumbo_content').hide();
+		jQuery('#ukm_page_jumbo_temp').html( clicked.attr('data-toggletitle') ).show();
+		
+		jQuery('#ukm_page_post_content').show();
+		jQuery('#pageDeFocus').attr('data-clicker', clicked.attr('id') ).html( clicked.attr('data-toggleclose') );
+    } else {
+        clicked.attr('data-action', 'show');
+
+		jQuery('#ukm_page_jumbo_temp').html('').hide();
+		jQuery('#ukm_page_jumbo_content').show();
+
+		jQuery('#ukm_page_pre_content').slideUp( 400, function(){jQuery('#ukm_page_content').show();} );
+		jQuery('#ukm_page_post_content').hide();
+		
+		jQuery('#ukm_page_jumbo_header_temp').hide();
+		jQuery('#ukm_page_jumbo_header').show();
+    }
+}
+
 window.onbeforeunload = function() {
 	//$('#page_load_content').fadeOut(400, function(){$('#page_load_loader').fadeIn();});
 };
@@ -15,4 +51,8 @@ $(document).on('touchend click', "a.isClicked", function(e) {
 $(document).on('submit', 'form', function(){
     $(this).find(':submit').attr('disabled', 'disabled').val('Vennligst vent...').html('Vennligst vent..');
     return true;
+});
+
+jQuery(document).ready(function(){
+	jQuery('textarea').autogrow();
 });
