27a4e735a5e9a3e37a5d891d9b670071f547d651
Marius Mandal <mariusmandal@gmail.com>
2015-11-30 23:27:57 +0100

    Fix checkbox jobbe på UKM

--- src/UKMNorge/DeltaBundle/Resources/public/js/triggerlink.js
+++ src/UKMNorge/DeltaBundle/Resources/public/js/triggerlink.js
@@ -5,16 +5,3 @@ jQuery(document).on('click', '.triggerLink', function() {
 jQuery(document).on('click', '.triggerClick:not( input, label)', function() {
 	document.getElementById( jQuery(this).find('.clickMe').attr('id') ).click();
 })
-
-
-$(document).on('touchend click', '.checkChildCheckbox', function(event) {
-	var target = $(event.target);
-	var checkbox = $(target).find('input.iAmChildCheckbox');
-	
-	if( !checkbox.prop('checked') ) {
-		checkbox.prop('checked','checked');
-	} else {
-		checkbox.prop('checked',false);
-		checkbox.removeProp('checked');
-	}
-});
