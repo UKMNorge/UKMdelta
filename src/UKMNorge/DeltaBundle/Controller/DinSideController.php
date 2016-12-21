698f0b39454063bdcc410cfacc95b73cd014507b
Marius Mandal <mariusmandal@gmail.com>
2016-12-21 10:23:27 +0100

    Lagt til støtte for ekstra-visninger

---
+++ src/UKMNorge/DeltaBundle/Controller/DinSideController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace UKMNorge\DeltaBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use DateTime;
+use person;
+use UKMCurl;
+
+class DinSideController extends Controller
+{
+	public function extrasAction()
+	{
+		$html = '';
+		
+		return new Response($html);
+	}
+	
+	
+	private function _season() {
+		return $this->get('ukm_delta.season')->getActive();
+	}
+}
