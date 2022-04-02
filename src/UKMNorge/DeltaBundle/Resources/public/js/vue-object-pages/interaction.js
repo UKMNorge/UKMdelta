deb9e0d6a70caa28a8a9e3f03ea5e4326ee3d831
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-04-02 21:03:07 +0200

    Metode for å vise meldinger til brukere
    En Vuejs komponent for å vise meldinger til brukere. denne metoden kan brukes hvis noe skjer, svar fra server som brukeren trenger å vite om (f.eks. sletting kan ikke utføres) eller umulige prosesser.

--- src/UKMNorge/DeltaBundle/Resources/public/js/vue-object-pages/interaction.js
+++ src/UKMNorge/DeltaBundle/Resources/public/js/vue-object-pages/interaction.js
@@ -1,4 +1,4 @@
-// Components
+// Dialog Components
 var dialogComponent = Vue.component('modaldialog-component', { 
     delimiters: ['#{', '}'], // For å bruke det på Twig
     props: {
@@ -47,6 +47,78 @@ var dialogComponent = Vue.component('modaldialog-component', {
             </div>
         </div>
     `
+});
+
+
+// Message Components
+var messageComponent = Vue.component('messagemodal-component', { 
+    delimiters: ['#{', '}'], // For å bruke det på Twig
+    props: {
+        alleInnslag : []
+    },
+    data : function() {
+        return {
+            melding : '',
+            title : '',
+            type : 0, // -1 error, 0 - normal, 1 - warning
+            open : false,
+            timeout : 0,
+            timeoutFunction : null
+        }
+    },
+    async mounted() {
+
+    },
+    methods : {
+        openMessage : function(title, melding, type = 0) {
+            if(this.timeoutFunction) {
+                clearTimeout(this.timeoutFunction);
+            }
+            
+            this.title = title
+            this.melding = melding;
+            this.type = type;
+
+            this.openModal(type);
+            
+            this.timeoutFunction = setTimeout(() => {
+                this.closeModal();
+            }, 5000);
+        },
+        openModal : function(type) {
+            if(this.open) {
+                this.closeModal();
+                setTimeout(() => {
+                    this.openMessage(this.title, this.melding, this.type);
+                }, 300);
+                return;
+            }
+
+            $('#mainMessageDiv').modal('show');
+            this.open = true;
+        },
+        closeModal : function() {
+            $('#mainMessageDiv').modal('hide');
+            this.open = false;
+        }
+    },
+    template : /*html*/`
+        <div class="modal fade" id="mainMessageDiv" tabindex="-1" role="dialog" data-backdrop="false" aria-hidden="false">
+            <div :class="{ 'error' : type == -1, 'warning' : type == 1}" class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content accordion-item with-shadow with-radius">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">#{title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        #{melding}
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
 })
 
 
@@ -63,7 +135,10 @@ var interactionVue = new Vue({
     methods : {
         openDialog : function(title, msg, buttons = null) {
             this.$refs.modalDialog.openDialog(title, msg, buttons);
+        },
+        showMessage : function(title, msg, type = 0) {
+            this.$refs.messageModal.openMessage(title, msg, type);
         }
     },
-    components : { 'dialog' : dialogComponent }
+    components : { 'dialog' : dialogComponent, 'message' : messageComponent }
 })
