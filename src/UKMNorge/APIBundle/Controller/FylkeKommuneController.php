c8b0b0c8238a164bac4e81550aa7e89866766b29
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-04-04 22:39:12 +0200

    Restrukturering: FylkeKommuneController

---
+++ src/UKMNorge/APIBundle/Controller/FylkeKommuneController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace UKMNorge\APIBundle\Controller;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use UKMNorge\Geografi\Fylker;
+
+
+require_once('UKM/Autoloader.php');
+
+class FylkeKommuneController extends SuperController {
+    
+    /* ---------------------------- Fylker ---------------------------- */
+
+    /**
+     * Hent et fylke med id
+     * @param string $fylke_id
+     * @return JsonResponse
+     */
+    public function getFylkeAction($fylke_id) {
+        $response = new JsonResponse();
+        
+        try{
+            return $response->setData(Fylker::getById($fylke_id));
+        } catch(Exception $e) {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setData($e->getMessage());
+            return $response;
+        }
+    }
+
+    /**
+     * Hent alle fylker
+     * @return JsonResponse
+     */
+    public function getAlleFylkerAction() {
+        $response = new JsonResponse();
+        
+        try{
+            return $response->setData(Fylker::getAll());
+        } catch(Exception $e) {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setData($e->getMessage());
+            return $response;
+        }
+    }
+
+    /* ---------------------------- Kommune ---------------------------- */
+    
+    /**
+     * Hent alle kommuner i et fylke ved å gi fylke id
+     * @param string $fylke_id
+     * @return JsonResponse
+     */
+    public function getAlleKommunerIFylkeAction($fylke_id) {
+        $response = new JsonResponse();
+        
+        try{
+            $fylke = Fylker::getById($fylke_id);
+            $kommuner = $fylke->getKommuner()->getAll();
+            
+            $kommuner_arr = [];
+            foreach($kommuner as $kommune) {
+                $kommuner_arr[] = [
+                    "id" => $kommune->getId(),
+                    "navn" => $kommune->getNavn(),
+                    'erAktiv' => $kommune->erAktiv(),
+                    'action' => $kommune->getAttr('action'),
+                    'link' => $kommune->getAttr('link'),
+                ];
+            }
+
+            return $response->setData($kommuner_arr);
+        } catch(Exception $e) {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setData($e->getMessage());
+            return $response;
+        }
+    }
+
+    /* ---------------------------- Fylker og kommuner ---------------------------- */
+    
+    /**
+     * Hent alle fylker og kommuner
+     * @return JsonResponse
+     */
+    public function getAlleFylkerOgKommunerAction() {
+        $response = new JsonResponse();
+
+        $fylker = [];
+
+        foreach(Fylker::getAll() as $fylke) {
+            $fylker[] = $fylke;
+
+            $kommuner_arr = [];
+            foreach($fylke->getKommuner()->getAll() as $kommune) {
+
+                $kommuner_arr[] = [
+                    "id" => $kommune->getId(),
+                    "navn" => $kommune->getNavn(),
+                    'erAktiv' => $kommune->erAktiv(),
+                    'action' => $kommune->getAttr('action'),
+                    'link' => $kommune->getLink(),
+                    'arrangementer_loaded' => false,
+                    'arrangementer' => []
+                ];
+            }
+
+            $fylke->kommuner = $kommuner_arr;
+            
+        }
+
+
+        try{
+            return $response->setData($fylker);
+        } catch(Exception $e) {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setData($e->getMessage());
+            return $response;
+        }
+    }
+}
