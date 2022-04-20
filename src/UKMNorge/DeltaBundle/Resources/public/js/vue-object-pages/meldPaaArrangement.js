393468ebe19ff7d9e9f06187617bbdfbd1c337c2
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-04-20 12:31:12 +0200

    Style

--- src/UKMNorge/DeltaBundle/Resources/public/js/vue-object-pages/meldPaaArrangement.js
+++ src/UKMNorge/DeltaBundle/Resources/public/js/vue-object-pages/meldPaaArrangement.js
@@ -173,7 +173,7 @@ var fylkerKommunerComponent = Vue.component('fylker-kommuner-component', {
                                                             <p class="info-label">#{ getDato(arrangement.frist_1) }</p>
                                                             <p v-if="arrangement.maksAntDeltagere != null && !arrangement.ventelisteLedigPlass" class="info-label venteliste">Venteliste</p>
                                                         </span>
-                                                        <svg v-show="arrangement.kommuner_fellesmonstring == null" style="height: 24px;width: 24px; margin: auto 0 auto auto;" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="#718096">
+                                                        <svg class="arrow-svg" v-show="arrangement.kommuner_fellesmonstring == null" style="height: 24px;width: 24px; margin: auto 0 auto auto;" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="#718096">
                                                             <path fill-rule="evenod d" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
                                                         </svg>
                                                     </div>
